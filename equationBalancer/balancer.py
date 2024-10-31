80d2255618b99c1a22e5b7fb406e22bf3ba655f5
Ondřej Kutil <kutil.ondra@outlook.cz>
2024-10-31 22:03:31 +0100

    Add equation balancer functionality with improved parsing and matrix handling
    - Introduced EquationBalancer class for managing equation parsing and balancing
    - Added error handling for invalid input in equation parsing
    - Created a user-friendly main interface for interacting with the balancer
    - Included requirements.txt for dependencies
    - Added .gitignore to exclude unnecessary files

--- equationBalancer/balancer.py
+++ equationBalancer/balancer.py
@@ -1,65 +1,125 @@
 # import libraries
-import re #re is a regular expresion library
+import re # regular expresion library
+import numpy as np
+from sympy import Matrix, lcm
 
+class EquationBalancer:
+    def __init__(self):
+        pass
 
+    def parseEquation(self, equation: str) -> tuple:
+        """
+        split the equation into reactants (left side) and products (right side)\n
+        eg.: parseEquation("H2+O2->H2O") return (["H2", "O2"], ["H2O"])
+        """
+        try:
+            reactants, products = equation.replace(' ', '').split('->')
+            reactants = reactants.split('+')
+            products = products.split('+')
 
-def parseEquation(equation: str) -> tuple:
-    """
-    split the equation into reactants (left side) and products (right side)\n
-    eg.: parseEquation("H2+O2->H2O") return (["H2", "O2"], ["H2O"])
+            # eg.: parseEquation("H2+O2->H2O") return (["H2", "O2"], ["H2O"])
+            return reactants, products
+        except Exception:
+            raise Exception('Invalid input. Please enter a valid equation.')
 
-    Args:
-        equation (str): string form of the equation
+    def parseMolecule(self, molecule: str) -> dict:
+        """
+        counts how many times an atom is present inside given molecule\n
+        eg.: C6H12O6 -> {'C': '6', 'H': '12', 'O': '6'}
+        """
+        
+        # returns dict with atom and it's count in the molecule
+        # [A-Z] - upper case followed by any number of lowecase symboles (denoted by star behind [a-z])
+        # (\d*) gives the number behind the string explained above, eg.: "H2" -> gives the 2
+        # re.findall() return a tuple
+        # r in front of a string means a raw string
+        # doesnt treat backslashes as an escape characters (eg. \n for new line or \t for tab)
+        atomPatternRe = r'([A-Z][a-z]*)(\d*)'
+        parsedMolecule = re.findall(atomPatternRe, molecule)
 
-    Returns:
-        tuple (reactants, products):
-    """
-    reactants, products = equation.split('->')
-    reactants = reactants.split('+')
-    products = products.split('+')
+        # creates a dictionary with atoms and its count in the molecule
+        atomCounts = {}
+        for atom, count in parsedMolecule:
+            count = int(count) if count else 1 # else 1 because re.findall() gives empty string ('') when atom is present only once
+            if atom in atomCounts:
+                atomCounts[atom] += count
+            else:
+                atomCounts[atom] = count
 
-    # eg.: parseEquation("H2+O2->H2O") return (["H2", "O2"], ["H2O"])
-    return reactants, products
+        return atomCounts
 
+    def getAtoms(self, reactants: list, products: list) -> list:
+        """
+        eg.: H2+O2->H2O returns ["H", "O"]
+        """
+        atoms = set()
+        for molecule in reactants + products:
+            # adds the atom to the set (.update())
+            # parseMolecule(x) gives me the eg. dict: {"H": "2"} so the .keys() gives us only the keys - eg.: {"H": "2"} -> "H"
+            atoms.update(self.parseMolecule(molecule=molecule).keys())
 
-def parseMolecule(molecule: str) -> dict:
-    """
-    counts how many times an atom is present inside given molecule\n
-    eg.: C6H12O6 -> [('C', '6'), ('H', '12'), ('O', '6')]
+        # returns list instead of set (easier to work with)
+        return sorted(atoms)
 
-    Args:
-        molecule (str):
-    
-    Returns:
-        dict: dictionary containing atoms and their count
-    """
-    
-    # returns list containing tuples with atom and it's count in the molecule, eg.: C6H12O6 -> [('C', '6'), ('H', '12'), ('O', '6')]
-    # [A-Z] - upper case followed by any number of lowecase symboles (denoted by star behind [a-z])
-    # (\d*) gives the number behind the string explained above, eg.: "H2" -> gives the 2
-    atomPatternRe = r'([A-Z][a-z]*)(\d*)'
-    parsedMolecule = re.findall(atomPatternRe, molecule)
+    def buildMatrix(self, reactants: list, products: list, atoms: list) -> list[list[int]]:
+        # gets how many atoms are present in given equation
+        numAtoms = len(atoms)
+        # how many molecules are in given equation
+        numMolecules = len(reactants) + len(products)
 
-    # creates a dictionary with atoms and its count in the molecule
-    atomCounts = {}
-    for atom, count in parsedMolecule:
-        count = int(count) if count else 1
-        if atom in atomCounts:
-            atomCounts[atom] += count
-        else:
-            atomCounts[atom] = count
+        # creates a matrix with rows = to how many atoms are in an equation and columns = to how many molecules are in an equation
+        # all 0s, all elements are int (dtype = int), else all elements would look like: 0. (. signaling it is a float (meaning 0.0))
+        matrix = np.zeros((numAtoms, numMolecules), dtype=int)
 
-    return atomCounts
+        for j, molecule in enumerate(reactants + products): # gives count (index j) and the coresponding value in the list
+            atomCounts = self.parseMolecule(molecule)
+            for i, atom in enumerate(atoms):
+                
+                # if j >= len(reactants) means that we are on the product side, thus need to multiply the count by -1
+                # .get(key, default value)
+                matrix[i, j] = atomCounts.get(atom, 0) * (-1 if j >= len(reactants) else 1)
 
+        return matrix
 
-def getAtoms(react: list, prod: list) -> list:
-    atoms = set()
-    for molecule in react + prod:
-        atoms.update(parseMolecule(molecule=molecule).keys())
+    def generateFinalSolutionString(self, reactants: list, products: list, solution: list) -> str:
+        #! ADD DOCSTRING
+        #! comment code
+        solutionString = ''
+        for i in range(len(reactants)):
+            solutionString = solutionString + f'{solution[i]} {reactants[i]}' + ' + '
+        solutionString = solutionString.rstrip(' + ')
+        solutionString = solutionString + ' -> '
+        for i in range(len(products)):
+            solutionString = solutionString + f'{solution[i + len(reactants)]} {products[i]}' + ' + '
+        solutionString = solutionString.rstrip(' + ')
+            
+        return solutionString
 
-    return sorted(atoms)
+    def balanceEquation(self, equation: str) -> tuple:
+        #! ADD DOCSTRING
+        #! explain why there is try except block, it is there co catch an error from any function called inside the try block
+        try:
+            reactants, products = self.parseEquation(equation=equation)
+            atoms = self.getAtoms(reactants, products)
+            matrix = self.buildMatrix(reactants, products, atoms)
 
+            # using sympy we can find the null space (= the solution)
+            nullSpace = Matrix(matrix).nullspace()
+            if not nullSpace:
+                return 'No solution'
+            else:
+                solution = nullSpace[0]
 
-def balanceEquation():
-    pass
+            # the solution may be in non-integer form, ie fraction
+            # we need to convert it to integers
+            # in sympy the denominator of a number is in .q
+            denominators = [number.q for number in solution]
+            # finds least common multiple of the denominators
+            lcmDenominator = lcm(denominators)
 
+            integerSolution = [number * lcmDenominator for number in solution]
+
+            #! explain the return statement, why is there the exit code 
+            return self.generateFinalSolutionString(reactants, products, integerSolution), 0
+        except Exception as e:
+            return f'Error: {e}', 1
--- equationBalancer/main.py
+++ equationBalancer/main.py
@@ -1,6 +1,47 @@
-from balancer import balanceEquation
-# main file to run
+from balancer import EquationBalancer
 
-print("+-----------------------------------------------+\n"
-      "|                                               |\n"
-      "|          Chemical equation balancer           |\n")
+#! add comments to the functions
+def display_welcome_screen():
+      print('Welcome to the Equation Balancer!')
+      print('This program helps you balance chemical equations.')
+      print('')
+      print('Instructions:')
+      print('1. Enter a chemical equation when prompted.')
+      print('2. Input is case sensitive.')
+      print('3. Use "->" to separate reactants and products.')
+      print('4. Use "+" to separate molecules.')
+      print('5. You can use spaces between molecules and atoms.')
+      print('')
+      print('Example input 1: H2 + O2 -> H2O')
+      print('Example input 2: NaCl + AgNO3 -> NaNO3 + AgCl')
+      print('')
+
+def prompt_user_for_equation():
+      return input('Enter your equation: ')
+
+display_welcome_screen()
+
+balancer = EquationBalancer()
+
+while True:
+    equation = prompt_user_for_equation()
+    solution, exitCode = balancer.balanceEquation(equation)
+    if exitCode == 0:
+        print('')
+        print(f'Balanced equation: {solution}')
+        print('')
+    else:
+        print('')
+        print(solution)
+        print('')
+        continue
+
+    while True:
+        another = input('Do you want to balance another equation? (y/n): ')
+        if another.lower() == 'n':
+            print('Thank you for using the Equation Balancer. Goodbye!')
+            exit(1)
+        elif another.lower() == 'y':
+            break
+        else:
+            print('Invalid input. Please enter "y" or "n".')
